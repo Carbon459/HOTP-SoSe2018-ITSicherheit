91ef5a638dc133d4fe4806a2a1828b590a34b83c
PLA7IN <konstantin.wacker@hotmail.de>
2018-04-24 18:02:43 +0200

    killed old comments

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,9 @@
 #include "lib\arduino-cc1101\cc1101.h"
 
 void setup() {
-  // put your setup code here, to run once.
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.println("Hahahah");
 }
